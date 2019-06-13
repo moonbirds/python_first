cd27ad7312f0aaaec406f613079546d8ee5cef17
markpunk <464537105@qq.com>
2019-06-14 00:29:05 +0800

    增加了分支结构的练习

--- Day01_homework.py
+++ Day01_homework.py
@@ -24,3 +24,36 @@ print('该股票的看涨期权的价格为：%.2f'% c) #C为看涨期权的价�
 p=k*math.exp(-r*T)*norm.cdf(-d_2)-s*norm.cdf(-d_1)
 print('该股票的看跌期权的价格为：%.2f'% p) #p为看跌期权的价格
 
+
+"""分支结构的练习"""
+"""GPA转换英国本科学位算法"""
+y2=float(input('第二年的平均成绩：'))
+y3=float(input('第三年的平均成绩：'))
+score=y2*0.4+y3*0.6#最终的加权平均分数
+if score < 40:
+    grade = 'No degree'#未能毕业
+elif score < 50:
+    grade = '3 class degree'
+elif score < 60:
+    grade = '2.2 class degree'
+elif score < 70:
+    grade = '2.1 class degree'
+else:
+    grade = '1 class degree'
+print(grade)
+
+
+"""人民币，美元，英镑，港币之间汇率换算"""
+value=float(input('货币总量：'))
+currency=input('货币种类为：')
+if currency == 'CNY' or currency == '人民币':
+    print('%.2f CNY= %.2f USD=%.2f GBP=%.2f HKD'% (value,value*0.1445,value*0.1140,value*1.1312))
+elif  currency == 'USD' or currency == '美元':
+    print('%.2f USD=%.2f CNY=%.2f GBP=%.2f HKD'%(value,value*6.9211,value*0.7887,value*7.8293))
+elif  currency == 'GBP' or currency == '英镑':
+    print('%.2f GBP=%.2f CNY=%.2f USD=%.2f HKD'%(value,value*8.7753,value*1.2679,value*9.9268))
+elif  currency == 'HKD' or currency == '港币':
+    print('%.2f HKD=%.2f CNY=%.2f USD=%.2f GBP'%(value,value*0.8840,value*0.1277,value*0.1007))
+else:
+    print('请输入有效货币单位')
+
